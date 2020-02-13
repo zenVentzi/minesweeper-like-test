4ad40b1d2f9cd1eff1f7e346fe3c07968b4dfd50
zenventzi <ventzislav.marinov@gmail.com>
2020-02-14 00:52:02 +0200

    comment visualization

--- types.d.ts
+++ types.d.ts
@@ -9,3 +9,35 @@ type Current = {
 };
 type Clicked = { location: Locationn; number: number };
 type Locationn = { row: number; col: number };
+
+/* 
+
+    [1, 1, 1, 1, 1]
+    [1, 1, 1, 0, 1]
+    [1, 1, 0, 1, 1]
+    [1, 1, 1, 1, 1]
+
+
+
+    [1, 1, 1]
+    [1, 1, 1]
+    [1, 1, 0]
+
+
+
+    [0, 1],
+    [1, 1],
+
+
+*/
+/* 
+Mark element as checked.
+Check if element is 0. 
+Yes? Recurse the function, passing the left element
+Yes? Go around the array and find adjacent elements.
+    For each adjacent element check if it is 0
+No? Do nothing.
+
+if end, return array with the indexes of the checked elements
+
+*/
